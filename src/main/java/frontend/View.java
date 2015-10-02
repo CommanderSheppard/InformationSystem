6cc11270befe40f0c43b66ce61ba09c7d985d071
CommanderSheppard <aryt9999@gmail.com>
2015-10-02 18:14:36 +0300

    minor updates
    -

--- src/main/java/frontend/View.java
+++ src/main/java/frontend/View.java
@@ -43,7 +43,7 @@ public class View extends UI {
     private void buildLayout() {
 
         createHeader();
-        createJPAView();
+      //  createJPAView();
 
         titleLayout = new HorizontalLayout(title);
         titleLayout.setSizeFull();
